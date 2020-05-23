758360c3d205150314fc7f78bd0c98d172e0812a
[muneneee] <[lukekevin39@gmail.com]>
2020-05-23 16:57:30 +0300

    created the location model

--- pics/models.py
+++ pics/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 class Photo(models.Model):
     image= models.ImageField(upload_to='photos/')
-    name = models.TextField()
+    name = models.CharField(max_length=30)
     description = models.TextField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     location = models.ForeignKey('Location' on_delete=models.CASCADE) 
+
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    location_name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.location_name
