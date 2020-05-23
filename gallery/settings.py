c4d44dcadc5aa834c435150bd7f68821701f02f1
[muneneee] <[lukekevin39@gmail.com]>
2020-05-23 11:15:13 +0300

    created templates  -created the base template,navbar,and index template  -defined views for the templates

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pics.apps.PicsConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
