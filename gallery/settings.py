2ad136add6e2ae7f3477a089976e2b34388785f5
[muneneee] <[lukekevin39@gmail.com]>
2020-05-25 02:11:17 +0300

    created procfile  -exported the variables to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nyx=@-ssc8p^kl)$n$q6-*j3+q#=7ltemr0qxen1y=9c3kgzj='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
