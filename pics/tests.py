04c5b9f3ea0e60c5b3680e4463ba9e0cf30f3abf
[muneneee] <[lukekevin39@gmail.com]>
2020-05-25 00:48:10 +0300

    instanciated and wrote test for photo class  -created test for saving the photo

--- pics/tests.py
+++ pics/tests.py
@@ -5,23 +5,20 @@ from .models import Photo,Location,Category
 class PhotoTestClass(TestCase):
 
     def setUp(self):
-        self.monalisa = Photo(image = 'yg.png', name='monalisa', description  = 'priceless art this is',posted = '2020-03-02')
-        self.monalisa.save_photo()
+        self.cat = Category(category_name="fashion")
+        self.cat.save()
 
-        self.new_category = Category(name = 'test')
-        self.new_category.save()
+        self.loc = Location(location_name="Africa")
+        self.loc.save()
 
-        self.new_location = Location(name = 'test')
-        self.new_location.save()
-
-        self.monalisa.Category.add(self.new_category)
-        self.monalisa.Location.add(self.new_location)    
+        self.image = Photo(name='image test', description='my test',location=self.loc,category=self.cat)
+        self.image.save()   
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.monalisa,Photo))
+        self.assertTrue(isinstance(self.image,Photo))
 
 
     def test_save_photo(self):
-        self.monalisa.save_photo()
+        self.image.save_photo()
         photos = Photo.objects.all()
         self.assertTrue(len(photos)>0)
