adc87d59a26bcb813529e92ecc0d943a0d705010
[muneneee] <[lukekevin39@gmail.com]>
2020-05-25 01:41:36 +0300

    configured database  -configured my database for deployment  -added the requirements

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'munene',
-        'PASSWORD': '12330122015',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_gallery'
+        }
+    },
 }
 
 
