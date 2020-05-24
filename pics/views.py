61b071a4fab864e625680fe04a483e61159a3c83
[muneneee] <[lukekevin39@gmail.com]>
2020-05-25 00:25:29 +0300

    defined a view for displaying a single article  -created a template for displaying single article

--- pics/views.py
+++ pics/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 from .models import Photo, Location
 
 # Create your views here.
@@ -31,3 +31,13 @@ def filter_location(request, location):
     title= f'{location} photos'
 
     return render(request,'location.html',{"title":title, "images":images, "locations":locations, "locatio":locatio})
+
+
+
+def photo(request,photo_id):
+    try:
+        photo = Photo.objects.get(id = photo_id)
+
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "pictures/photo.html", {"photo":photo})
