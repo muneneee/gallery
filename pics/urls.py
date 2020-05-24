1c454528cd32ba14a2369a01dabdfe1bdc21dcfe
[muneneee] <[lukekevin39@gmail.com]>
2020-05-25 00:20:12 +0300

    defined a new url for viewing a single photo

--- pics/urls.py
+++ pics/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 urlpatterns=[
     url('^$',views.index,name='home'),
     url('search/',views.search_results, name='search_results'),
-    url(r'^location/(\d+)',views.filter_location,name='filter_location')
+    url(r'^location/(\d+)',views.filter_location,name='filter_location'),
+    url(r'^photo/(\d+)',views.photo,name='photo')
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
